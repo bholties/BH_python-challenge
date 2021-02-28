9ff952c8b58eb0b72abdcd14a0c6ec91a046aefd
Bholties <77474724+bholties@users.noreply.github.com>
2021-02-27 21:54:37 -0600

    Add files via upload
    Python Poll Results

--- file.main.py
+++ file.main.py
@@ -2,40 +2,60 @@ import os
 import csv
 
 # Declare file location 
-data_output = os.path.join("", "Py_Bank", "budget_data.csv")
+data_output = os.path.join("", "Py_Poll", "election_data.csv")
 print(data_output)
 
 # iterate through rows for the following variables
-total_months = []
-total_amount = []
-average_change = []
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
-with open(r"C:\Users\ramr3_000\Desktop\Python Homework\BH_python-challenge\Py_Bank\budget_data.csv", "r") as csvfile:
+with open(r"C:\Users\ramr3_000\Desktop\Python Homework\BH_python-challenge\Py_Poll\election_data.csv", "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)  
 
 
-# Iterate through the rows 
+    # Iterate through the rows 
     for row in csvreader: 
+        
+        total_votes +=1
 
-# Append the total months and total amount
-        total_months.append(row[0])
-        total_amount.append(int(row[1])) 
+        # We have four candidates 
+        
+        if row[2] == "Khan": 
+            khan_votes +=1
+        elif row[2] == "Correy":
+            correy_votes +=1
+        elif row[2] == "Li": 
+            li_votes +=1
+        elif row[2] == "O'Tooley":
+            otooley_votes +=1
 
-# How to get monthly change in profits
-for i in range(1,len(total_amount)):
-    average_change.append(total_amount[i] - (total_amount[i-1])
-    
-max_increase_value = max(average_change)
-min_decrease_value = min(average_change)
+ 
+candidates = ["Khan", "Correy", "Li","O'Tooley"]
+votes = [khan_votes, correy_votes,li_votes,otooley_votes]
 
-max_increase_monthly = average_change.index(max(average_change)) + 1
-max_decrease_monthly = average_change.index(min(average_change)) + 1
 
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total Months: {len(total_months)}")
-print(f"Total: ${sum(total_amount)}")
-print(f"Average Change: {round(sum(average_change)/len(average_change),2)}")
-print(f"Greatest Increase in Profits: {total_months[max_increase_monthly]} (${(str(max_increase_value))})")
-print(f"Greatest Decrease in Profits: {total_months[max_decrease_monthly]} (${(str(max_decrease_value))})")   
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
+
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
+
+# Print the summary table
+print(f"Election Results")
+print(f"----------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"----------------------------")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"----------------------------")
+print(f"Winner: {key}")
+print(f"----------------------------")
